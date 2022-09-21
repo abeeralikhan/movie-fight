5b6d0d32d0c779dadf1abc3bac6eea88e2e1c86b
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-09-22 00:26:42 +0500

    feat: add a dropdown menu

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ API params
   s --> for index search (list) --> pass movie name
   i --> for show search (lookup) --> pass movie id
 */
+
 // helper function: for index search or to get partial data of movies
 const fetchData = async (searchQuery) => {
   const response = await axios.get("http://www.omdbapi.com/", {
