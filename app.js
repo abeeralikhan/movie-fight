37f8eb0eb59f379354b955130a56d88c33b15ccf
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-09-21 18:26:10 +0500

    feat: fetch movie data

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+// helper function
+const fetchData = async () => {
+  const response = await axios.get("http://www.omdbapi.com/?", {
+    params: {
+      apikey: "96f77f16",
+      s: "avengers",
+    },
+  });
+
+  console.log(response.data);
+};
+
+fetchData();
