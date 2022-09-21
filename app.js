3875f151a9448a0086fe4278dcb66c598f4cfcf2
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-09-21 21:09:28 +0500

    feat: delay search input

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
-// helper function
-const fetchData = async () => {
+/* 
+API params
+  s --> for index search (list) --> pass movie name
+  i --> for show search (lookup) --> pass movie id
+*/
+// helper function: for index search or to get partial data of movies
+const fetchData = async (searchQuery) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "96f77f16",
-      s: "avengers",
+      s: searchQuery,
     },
   });
 
   console.log(response.data);
 };
 
-fetchData();
+let timerID;
+const searchBar = document.querySelector(".searchBar");
+
+const onInput = (event) => {
+  if (timerID) {
+    clearInterval(timerID);
+  }
+
+  timerID = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 500);
+};
+
+searchBar.addEventListener("input", onInput);
