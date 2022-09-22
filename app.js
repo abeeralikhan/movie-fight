bcf160555b3bb96bf4cdc5c76d3b5b47eb29ce10
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-09-22 19:22:06 +0500

    refact: extract the autocomplete logic

--- app.js
+++ app.js
@@ -24,70 +24,16 @@ const fetchData = async (searchQuery) => {
   return response.data.Search;
 };
 
-const root = document.querySelector(".autocomplete");
+createAutoComplete({
+  root: document.querySelector(".autocomplete1"),
+});
 
-root.innerHTML = `
-  <label><strong> Search For a Movie </strong></label>
-  <input class="searchBar input" type="text"/>
-  <div class="dropdown">
-    <div class"dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
+createAutoComplete({
+  root: document.querySelector(".autocomplete2"),
+});
 
-const searchBar = document.querySelector(".searchBar");
-const dropdown = document.querySelector(".dropdown");
-const resultsWrapper = document.querySelector(".results");
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-
-  // for handling empty responses
-  if (!movies.length) {
-    // hide dropdown menu if not found any searched movie title
-    dropdown.style.display = "none";
-    return;
-  }
-
-  // for clearing previous search results
-  resultsWrapper.innerHTML = "";
-
-  // for making the dropdown menu visible again
-  // we are setting the display to none on external clicks & on empty responses
-  dropdown.style.display = "block";
-
-  for (let movie of movies) {
-    const option = document.createElement("a");
-
-    // if poster of the movie is unavailable i.e "N/A",
-    // setting it equal to an empty string to avoid errors
-    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
-
-    option.classList.add("dropdown-item");
-    option.innerHTML = `
-      <img src="${imgSrc}" />
-      ${movie.Title}
-    `;
-    option.addEventListener("click", () => {
-      dropdown.style.display = "none";
-      searchBar.value = movie.Title;
-      onMovieSelect(movie);
-    });
-
-    resultsWrapper.appendChild(option);
-  }
-};
-
-// wrapping the onInput function inside the debounce function,
-// and passing setting the delay equal to 500ms i.e 0.5s
-searchBar.addEventListener("input", debounce(onInput, 500));
-
-document.addEventListener("click", (event) => {
-  if (!root.contains(event.target)) {
-    // hide the dropdown menu on external clicks
-    dropdown.style.display = "none";
-  }
+createAutoComplete({
+  root: document.querySelector(".autocomplete3"),
 });
 
 const onMovieSelect = async (movie) => {
